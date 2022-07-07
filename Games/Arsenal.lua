436a2a70c30b6ac64faf6b5b19394ca659c215a1
Ghost-X Collective <108462714+ProxColdAirPublishing@users.noreply.github.com>
2022-07-07 18:37:32 +1000

    Create Arsenal.lua

---
+++ Games/Arsenal.lua
@@ -0,0 +1,13 @@
+	local AkaliNotif = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/Dynissimo/main/Scripts/AkaliNotif.lua"))();
+			Notify({
+				Description = "Support for this game is still a work in progress.";
+				Title = "GHOST-X";
+				Duration = 6;
+			});
+task.wait(1)
+			Notify({
+				Description = "Join our discord server for more updates! (Copied To Your Clipboard)";
+				Title = "GHOST-X";
+				Duration = 10;
+			});
+setclipboard("https://discord.gg/xJ44u5dXDK")
