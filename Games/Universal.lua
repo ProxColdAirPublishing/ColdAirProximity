42427cac0268b3e59526819e97ef241dcdcee9c9
Ghost-X Collective <108462714+ProxColdAirPublishing@users.noreply.github.com>
2022-07-14 14:09:06 +1000

    Update Universal.lua

--- Games/Universal.lua
+++ Games/Universal.lua
@@ -11,17 +11,4 @@ task.wait(1)
 				Title = "GHOST-X";
 				Duration = 10;
 			});
-setclipboard("https://discord.gg/xJ44u5dXDK")local AkaliNotif = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/Dynissimo/main/Scripts/AkaliNotif.lua"))();
-local Notify = AkaliNotif.Notify;
-			Notify({
-				Description = "Support for this game is still a work in progress.";
-				Title = "GHOST-X";
-				Duration = 6;
-			});
-task.wait(1)
-			Notify({
-				Description = "Join our discord server for more updates! (Copied To Your Clipboard)";
-				Title = "GHOST-X";
-				Duration = 10;
-			});
 setclipboard("https://discord.gg/xJ44u5dXDK")
